da8b3142ef3d01a4e88e85cf1315b6775641cbce
Cody Rushing <codyrushing@gmail.com>
2026-09-12 15:33:48 -0500

    Adding images to cards. Adding covid dashhboard

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -10,5 +10,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), solidJs()]
+  integrations: [mdx(), solidJs()],
+  image: {
+    layout: "constrained",
+  }
 });
